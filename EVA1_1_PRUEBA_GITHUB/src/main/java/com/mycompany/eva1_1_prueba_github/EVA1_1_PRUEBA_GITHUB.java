d0cbd5ac42f65bdab27e0582e47de47acb7096f9
trejo09g <104469081+trejo09g@users.noreply.github.com>
2023-08-23 18:27:43 -0600

    Add files via upload

---
+++ EVA1_1_PRUEBA_GITHUB/src/main/java/com/mycompany/eva1_1_prueba_github/EVA1_1_PRUEBA_GITHUB.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_1_prueba_github;
+
+/**
+ *
+ * @author garci
+ */
+public class EVA1_1_PRUEBA_GITHUB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
