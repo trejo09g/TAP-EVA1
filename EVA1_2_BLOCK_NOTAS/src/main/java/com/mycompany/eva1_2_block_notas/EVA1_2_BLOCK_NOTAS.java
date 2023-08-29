e0b1ce58db2e0cbbbf814ce07ee13cf02c0869de
trejo09g <104469081+trejo09g@users.noreply.github.com>
2023-08-28 20:27:11 -0600

    Add files via upload

---
+++ EVA1_2_BLOCK_NOTAS/src/main/java/com/mycompany/eva1_2_block_notas/EVA1_2_BLOCK_NOTAS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_2_block_notas;
+
+/**
+ *
+ * @author garci
+ */
+public class EVA1_2_BLOCK_NOTAS {
+
+    public static void main(String[] args) {
+        
+    }
+}
