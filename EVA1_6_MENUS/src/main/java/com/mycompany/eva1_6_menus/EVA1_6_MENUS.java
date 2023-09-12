02b3707b3b9e8c5da07d55a73f279e791b058e0e
trejo09g <104469081+trejo09g@users.noreply.github.com>
2023-09-11 18:48:35 -0600

    Add files via upload

---
+++ EVA1_6_MENUS/src/main/java/com/mycompany/eva1_6_menus/EVA1_6_MENUS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_6_menus;
+
+/**
+ *
+ * @author garci
+ */
+public class EVA1_6_MENUS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
