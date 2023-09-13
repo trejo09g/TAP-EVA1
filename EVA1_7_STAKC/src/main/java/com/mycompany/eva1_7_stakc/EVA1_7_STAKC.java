0ad3adec7d48e73f8558f54c808d9df008d1b5c1
trejo09g <104469081+trejo09g@users.noreply.github.com>
2023-09-13 17:01:07 -0600

    Add files via upload

---
+++ EVA1_7_STAKC/src/main/java/com/mycompany/eva1_7_stakc/EVA1_7_STAKC.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_7_stakc;
+
+/**
+ *
+ * @author garci
+ */
+public class EVA1_7_STAKC {
+
+    public static void main(String[] args) {
+        System.out.println("Inicia MAIN");
+        A();
+        System.out.println("Fin MAIN");
+    }
+    public static void A(){
+        System.out.println("Inicia A");
+        B();
+        System.out.println("Fin A");
+        
+    }
+    public static void B(){
+        System.out.println("Inicia B");
+        System.out.println("Fin B");
+        
+    }
+}
