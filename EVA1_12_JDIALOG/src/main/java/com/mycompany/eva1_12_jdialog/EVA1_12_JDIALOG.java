1a71e600b6dd52d4cb5751ee78e07058a0ff776d
trejo09g <104469081+trejo09g@users.noreply.github.com>
2023-09-19 22:44:13 -0600

    Add files via upload

---
+++ EVA1_12_JDIALOG/src/main/java/com/mycompany/eva1_12_jdialog/EVA1_12_JDIALOG.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_12_jdialog;
+
+/**
+ *
+ * @author garci
+ */
+public class EVA1_12_JDIALOG {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
